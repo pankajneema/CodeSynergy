bf5f69624d140484d923d8d6196dfc117cbb0b46
pankajneema <pankaj200321@gmail.com>
2024-06-03 03:01:04 +0530

    Your commit message

--- js/contact.js
+++ js/contact.js
@@ -1,73 +1,58 @@
-$(function() {
 
-	'use strict';
 
-	// Form
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    alert("start");
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var subject = document.getElementById("subject").value;
+    var message = document.getElementById("message").value;
+    console.log("your name +",name);
+	console.log("your email +",email);
+	console.log("your subject +",subject);
+	console.log("your message +",message);
 
-	var contactForm = function() {
-            
+    if (subject === '') {
+        subject = "Contact Form Submission";
+    }
 
-		if ($('#contactForm').length > 0 ) {
-			
-			$( "#contactForm" ).validate( {
-				rules: {
-					name: "required",
-					email: {
-						required: true,
-						email: true
-					},
-					message: {
-						required: true,
-						minlength: 5
-					}
-				},
-				messages: {
-					name: "Please enter your name",
-					email: "Please enter a valid email address",
-					message: "Please enter a message"
-				},
+    var textMessage = "Email from: " + name + "\nEmail address: " + email + "\nEmail Subject: " + subject + "\nMessage: \n" + message;
+    var htmlMessage = "Email from: " + name + "<br />Email address: " + email + "<br />Message: " + message.replace(/\n/g, "<br />");
+    htmlMessage += "<br /> ---  --- <br />";
 
-				
-				/* submit via ajax */
-				submitHandler: function(form) {		
-					var $submit = $('.submitting'),
-						waitText = 'Submitting...';
+    var postData = {
+        from: { email: 'MS_MQJxca@trial-zr6ke4nj7jmgon12.mlsender.net' },
+        to: [{ email: 'pankaj200321@gmail.com' }],
+        subject: "Incoming From Portfolio - " + subject,
+        text: textMessage,
+        html: htmlMessage
+    };
 
-					$.ajax({   	
-				      type: "POST",
-				      url: "php/send-email.php",
-				      data: $(form).serialize(),
-
-				      beforeSend: function() { 
-				      	$submit.css('display', 'block').text(waitText);
-				      },
-				      success: function(msg) {
-		               if (msg == 'OK') {
-		               	$('#form-message-warning').hide();
-				            setTimeout(function(){
-		               		$('#contactForm').fadeOut();
-		               	}, 1000);
-				            setTimeout(function(){
-				               $('#form-message-success').fadeIn();   
-		               	}, 1400);
-			               
-			            } else {
-			               $('#form-message-warning').html(msg);
-				            $('#form-message-warning').fadeIn();
-				            $submit.css('display', 'none');
-			            }
-				      },
-				      error: function() {
-				      	$('#form-message-warning').html("Something went wrong. Please try again.");
-				         $('#form-message-warning').fadeIn();
-				         $submit.css('display', 'none');
-				      }
-			      });    		
-		  		}
-				
-			} );
-		}
-	};
-	contactForm();
-
+    fetch('https://api.mailersend.com/v1/email', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + 'mlsn.9cb07ac92c343359c5e50dbde15075163a9a1b574b1cee90e77aff2281f65dd6' // Replace with your MailerSend API token
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        if (data.message === 'OK') {
+            document.getElementById("form-message-success").style.display = "block";
+            document.getElementById("form-message-success").innerText = "Your message was sent, thank you!";
+            document.getElementById("contactForm").reset();
+        } else {
+            document.getElementById("form-message-warning").style.display = "block";
+            document.getElementById("form-message-warning").innerText = "Something went wrong. Please try again later.";
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById("form-message-warning").style.display = "block";
+        document.getElementById("form-message-warning").innerText = "Something went wrong. Please try again later.";
+    });
 });
+
+
