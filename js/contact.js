fef76afaf79b060f038092f4feade794df357e60
pankajneema <pankaj200321@gmail.com>
2024-08-02 01:07:28 +0530

    Fix EMail Signed-off-by: pankajneema <pankaj200321@gmail.com>

--- js/contact.js
+++ js/contact.js
@@ -1,58 +1,46 @@
-
-
 document.getElementById("contactForm").addEventListener("submit", function(event) {
     event.preventDefault();
-    alert("start");
+    // alert("Hey Email Sending Start");
+
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
-    var subject = document.getElementById("subject").value;
+    var subject = document.getElementById("subject").value || "Contact Form Submission";
     var message = document.getElementById("message").value;
-    console.log("your name +",name);
-	console.log("your email +",email);
-	console.log("your subject +",subject);
-	console.log("your message +",message);
 
-    if (subject === '') {
-        subject = "Contact Form Submission";
-    }
+    console.log("your name:", name);
+    console.log("your email:", email);
+    console.log("your subject:", subject);
+    console.log("your message:", message);
 
     var textMessage = "Email from: " + name + "\nEmail address: " + email + "\nEmail Subject: " + subject + "\nMessage: \n" + message;
     var htmlMessage = "Email from: " + name + "<br />Email address: " + email + "<br />Message: " + message.replace(/\n/g, "<br />");
     htmlMessage += "<br /> ---  --- <br />";
 
-    var postData = {
-        from: { email: 'MS_MQJxca@trial-zr6ke4nj7jmgon12.mlsender.net' },
-        to: [{ email: 'pankaj200321@gmail.com' }],
-        subject: "Incoming From Portfolio - " + subject,
-        text: textMessage,
-        html: htmlMessage
-    };
-
-    fetch('https://api.mailersend.com/v1/email', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer ' + 'mlsn.9cb07ac92c343359c5e50dbde15075163a9a1b574b1cee90e77aff2281f65dd6' // Replace with your MailerSend API token
-        },
-        body: JSON.stringify(postData)
+    Email.send({
+        Host: "smtp.elasticemail.com",
+        Username: "codesynergy@elasticsearch.com",
+        Password: "0AEA789AFE444CF02B220C14826611E9FC99",
+        To: 'smtpserver391@gmail.com',
+        From: "pankaj200321@gmail.com",
+        Subject:  "Mail From CODESYNERGY =>" + subject,
+        Body: htmlMessage
     })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data);
-        if (data.message === 'OK') {
-            document.getElementById("form-message-success").style.display = "block";
-            document.getElementById("form-message-success").innerText = "Your message was sent, thank you!";
-            document.getElementById("contactForm").reset();
-        } else {
-            document.getElementById("form-message-warning").style.display = "block";
-            document.getElementById("form-message-warning").innerText = "Something went wrong. Please try again later.";
-        }
+    .then(function (message) {
+        document.getElementById("form-message-success").style.display = "block";
+        document.getElementById("form-message-success").innerText = "Your message was sent, thank you!";
+        document.getElementById("contactForm").reset();
+        setTimeout(function() {
+            document.getElementById("form-message-success").innerText = "";
+        }, 5000);
+    
     })
     .catch(error => {
         console.error('Error:', error);
         document.getElementById("form-message-warning").style.display = "block";
         document.getElementById("form-message-warning").innerText = "Something went wrong. Please try again later.";
+        setTimeout(function() {
+            document.getElementById("form-message-warning").innerText = "";
+        }, 5000);
     });
+
 });
-
-
